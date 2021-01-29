4c730a1e5fd4df69b6b881c33316615170633703
Tony Arcieri <bascule@gmail.com>
2021-01-29 13:32:27 -0800

    scrypt: rename ScryptParams => Params
    This follows RFC 356 naming conventions:
    
    https://rust-lang.github.io/rfcs/0356-no-module-prefixes.html

--- scrypt/tests/mod.rs
+++ scrypt/tests/mod.rs
@@ -1,4 +1,4 @@
-use scrypt::{scrypt, ScryptParams};
+use scrypt::{scrypt, Params};
 
 #[cfg(feature = "simple")]
 use {
@@ -69,7 +69,7 @@ fn test_scrypt() {
     let tests = tests();
     for t in tests.iter() {
         let mut result = vec![0u8; t.expected.len()];
-        let params = ScryptParams::new(t.log_n, t.r, t.p).unwrap();
+        let params = Params::new(t.log_n, t.r, t.p).unwrap();
         scrypt(
             t.password.as_bytes(),
             t.salt.as_bytes(),
