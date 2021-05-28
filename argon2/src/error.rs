f6baa3bc129a10723d2e4e626f874608f950022c
Tony Arcieri <bascule@gmail.com>
2021-05-28 09:23:36 -0700

    argon2: error handling improvements
    Adds a proper conversion between argon2::Error and password_hash::Error
    and uses it when hashing the password.

--- argon2/src/error.rs
+++ argon2/src/error.rs
@@ -3,7 +3,7 @@
 use core::fmt;
 
 /// Error type.
-// TODO(tarcieri): finer-grained context-specific errors?
+// TODO(tarcieri): consolidate/replace with `password_hash::Error`
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Error {
     /// Associated data is too long
@@ -75,5 +75,30 @@ impl fmt::Display for Error {
             Error::TimeTooSmall => "time cost is too small",
             Error::VersionInvalid => "invalid version",
         })
+    }
+}
+
+#[cfg(feature = "password-hash")]
+#[cfg_attr(docsrs, doc(cfg(feature = "password-hash")))]
+impl From<Error> for password_hash::Error {
+    fn from(err: Error) -> password_hash::Error {
+        match err {
+            Error::AdTooLong => password_hash::Error::ParamValueInvalid,
+            Error::AlgorithmInvalid => password_hash::Error::Algorithm,
+            Error::LanesTooFew => password_hash::Error::ParamValueInvalid,
+            Error::LanesTooMany => password_hash::Error::ParamValueInvalid,
+            Error::MemoryTooLittle => password_hash::Error::ParamValueInvalid,
+            Error::MemoryTooMuch => password_hash::Error::ParamValueInvalid,
+            Error::PwdTooLong => password_hash::Error::Password,
+            Error::OutputTooShort => password_hash::Error::OutputTooShort,
+            Error::OutputTooLong => password_hash::Error::OutputTooLong,
+            Error::SaltTooShort => password_hash::Error::SaltTooLong,
+            Error::SaltTooLong => password_hash::Error::SaltTooLong,
+            Error::SecretTooLong => password_hash::Error::ParamValueInvalid,
+            Error::ThreadsTooFew => password_hash::Error::ParamValueInvalid,
+            Error::ThreadsTooMany => password_hash::Error::ParamValueInvalid,
+            Error::TimeTooSmall => password_hash::Error::ParamValueInvalid,
+            Error::VersionInvalid => password_hash::Error::Version,
+        }
     }
 }
