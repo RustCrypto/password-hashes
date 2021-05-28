401542985f2f4bee3b0af933109845177858637f
Tony Arcieri <bascule@gmail.com>
2021-05-28 11:40:41 -0700

    argon2: make `Params` always available; fix `hash_password_simple`
    Closes #179.
    
    This commit makes the `Params` struct always available regardless of
    enabled crate features, adding on functionality when the `password-hash`
    feature is enabled.
    
    Per the added TODOs in this commit, it really seems like in the next
    breaking release, `Argon2::new` should accept `Params` as an argument
    rather than duplicating all of the individual parameter fields and
    passing them in as arguments. After that, they can be validated and
    stored within the `Argon2` struct itself. But that would be a breaking
    change, so for now they're stored piecemeal.
    
    Additionally, this commit now ensures enough context is available in the
    `Argon2` struct in order to implement `hash_password_simple` that uses
    the configured params rather than the defaults, which addresses the
    aforementioned issue.

--- argon2/src/params.rs
+++ argon2/src/params.rs
@@ -1,51 +1,69 @@
 //! Argon2 password hash parameters.
 
-use crate::{Argon2, Version};
-use core::convert::{TryFrom, TryInto};
-use password_hash::{Decimal, ParamsString, PasswordHash};
+use crate::Version;
+
+#[cfg(feature = "password-hash")]
+use {
+    core::convert::{TryFrom, TryInto},
+    password_hash::{ParamsString, PasswordHash},
+};
 
 /// Argon2 password hash parameters.
 ///
 /// These are parameters which can be encoded into a PHC hash string.
-#[cfg_attr(docsrs, doc(cfg(feature = "password-hash")))]
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Params {
     /// Memory size, expressed in kilobytes, between 1 and (2^32)-1.
     ///
     /// Value is an integer in decimal (1 to 10 digits).
-    pub m_cost: Decimal,
+    pub m_cost: u32,
 
     /// Number of iterations, between 1 and (2^32)-1.
     ///
     /// Value is an integer in decimal (1 to 10 digits).
-    pub t_cost: Decimal,
+    pub t_cost: u32,
 
     /// Degree of parallelism, between 1 and 255.
     ///
     /// Value is an integer in decimal (1 to 3 digits).
-    pub p_cost: Decimal,
+    pub p_cost: u32,
 
     /// Size of the output (in bytes)
     pub output_size: usize,
 
     /// Algorithm version
+    // TODO(tarcieri): make this separate from params in the next breaking release?
     pub version: Version,
+}
+
+impl Params {
+    /// Default memory cost.
+    pub const DEFAULT_M_COST: u32 = 4096;
+
+    /// Default number of iterations.
+    pub const DEFAULT_T_COST: u32 = 3;
+
+    /// Default degree of parallelism.
+    pub const DEFAULT_P_COST: u32 = 1;
+
+    /// Default output size.
+    pub const DEFAULT_OUTPUT_SIZE: usize = 32;
 }
 
 impl Default for Params {
     fn default() -> Params {
-        let ctx = Argon2::default();
-
         Params {
-            m_cost: ctx.m_cost,
-            t_cost: ctx.t_cost,
-            p_cost: ctx.threads,
-            output_size: 32,
+            m_cost: Self::DEFAULT_M_COST,
+            t_cost: Self::DEFAULT_T_COST,
+            p_cost: Self::DEFAULT_P_COST,
+            output_size: Self::DEFAULT_OUTPUT_SIZE,
             version: Version::default(),
         }
     }
 }
 
+#[cfg(feature = "password-hash")]
+#[cfg_attr(docsrs, doc(cfg(feature = "password-hash")))]
 impl<'a> TryFrom<&'a PasswordHash<'a>> for Params {
     type Error = password_hash::Error;
 
@@ -77,6 +95,8 @@ impl<'a> TryFrom<&'a PasswordHash<'a>> for Params {
     }
 }
 
+#[cfg(feature = "password-hash")]
+#[cfg_attr(docsrs, doc(cfg(feature = "password-hash")))]
 impl<'a> TryFrom<Params> for ParamsString {
     type Error = password_hash::Error;
 
