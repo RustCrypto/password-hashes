5080349bd053c069e16bd4bf839ccd7130651453
Tony Arcieri <bascule@gmail.com>
2021-08-27 17:41:20 -0600

    Remove `McfHasher` impls for `pbkdf2` and `scrypt` crates (#219)
    The Modular Crypt Format is a legacy format, and the implementations
    provided in the `pbkdf2` and `scrypt` crates implement a bespoke
    RustCrypto-specific format.
    
    Since that format has been introduced, support has been added to both
    crates for the newer PHC String Format:
    
    https://github.com/P-H-C/phc-string-format/blob/master/phc-sf-spec.md
    
    See the "Usage" rustdoc for the respective crates for information on how
    to use the PHC string format.
    
    Previous releases of these respective crates support converting from the
    MCF hash format to the PHC format using `McfHasher::upgrade_mcf_hash`:
    
    https://docs.rs/password-hash/0.3.0/password_hash/trait.McfHasher.html#tymethod.upgrade_mcf_hash
    
    Before upgrading these crates to the new release which removes MCF hash
    support, please use the older releases to upgrade all of your password
    hashes in MCF format to the new PHC format.

--- pbkdf2/tests/simple.rs
+++ pbkdf2/tests/simple.rs
@@ -6,7 +6,7 @@
 
 use hex_literal::hex;
 use pbkdf2::{
-    password_hash::{McfHasher, PasswordHasher, Salt},
+    password_hash::{PasswordHasher, Salt},
     Algorithm, Params, Pbkdf2,
 };
 use std::convert::TryFrom;
@@ -39,20 +39,4 @@ fn hash_with_default_algorithm() {
 
     let expected_output = hex!("c5e478d59288c841aa530db6845c4c8d962893a001ce4e11a4963873aa98134a");
     assert_eq!(hash.hash.unwrap().as_ref(), expected_output);
-}
-
-#[test]
-fn upgrade_mcf_hash() {
-    let mcf_hash = "$rpbkdf2$0$AAAEAA==$w7Y1w07wETYY7CXw5W07TA==$wRwXwI/764oNt1HvTeQcIrqr9rfzfq/KySgcCROy1HU=$";
-    let phc_hash = Pbkdf2.upgrade_mcf_hash(&mcf_hash).unwrap();
-
-    assert_eq!(phc_hash.algorithm, Algorithm::Pbkdf2Sha256.ident());
-
-    let params = Params::try_from(&phc_hash).unwrap();
-    assert_eq!(params.rounds, 1024);
-    assert_eq!(params.output_length, 32);
-    assert_eq!(
-        Pbkdf2.verify_mcf_hash(PASSWORD.as_bytes(), &mcf_hash),
-        Ok(())
-    );
 }
--- scrypt/src/simple.rs
+++ scrypt/src/simple.rs
@@ -1,12 +1,8 @@
 //! Implementation of the `password-hash` crate API.
 
 use crate::{scrypt, Params};
-use base64ct::{Base64, Encoding};
 use core::convert::TryInto;
-use password_hash::{
-    errors::InvalidValue, Decimal, Error, Ident, McfHasher, Output, PasswordHash, PasswordHasher,
-    Result, Salt,
-};
+use password_hash::{Decimal, Error, Ident, Output, PasswordHash, PasswordHasher, Result, Salt};
 
 /// Algorithm identifier
 pub const ALG_ID: Ident = Ident::new("scrypt");
@@ -55,63 +51,4 @@ impl PasswordHasher for Scrypt {
             hash: Some(output),
         })
     }
-}
-
-impl McfHasher for Scrypt {
-    fn upgrade_mcf_hash<'a>(&self, hash: &'a str) -> Result<PasswordHash<'a>> {
-        let mut parts = hash.split('$');
-
-        let buf = [
-            parts.next(),
-            parts.next(),
-            parts.next(),
-            parts.next(),
-            parts.next(),
-            parts.next(),
-            parts.next(),
-            parts.next(),
-        ];
-
-        let (log_n, r, p, salt, hash) = match buf {
-            [Some(""), Some("rscrypt"), Some("0"), Some(p), Some(s), Some(h), Some(""), None] => {
-                let pvec = Base64::decode_vec(p)?;
-                if pvec.len() != 3 {
-                    return Err(InvalidValue::Malformed.param_error());
-                }
-                (pvec[0], pvec[1] as u32, pvec[2] as u32, s, h)
-            }
-            [Some(""), Some("rscrypt"), Some("1"), Some(p), Some(s), Some(h), Some(""), None] => {
-                let pvec = Base64::decode_vec(p)?;
-                if pvec.len() != 9 {
-                    return Err(InvalidValue::Malformed.param_error());
-                }
-                let log_n = pvec[0];
-                let r = u32::from_le_bytes(pvec[1..5].try_into().unwrap());
-                let p = u32::from_le_bytes(pvec[5..9].try_into().unwrap());
-                (log_n, r, p, s, h)
-            }
-            _ => return Err(InvalidValue::Malformed.param_error()),
-        };
-
-        let params = Params::new(log_n, r, p).map_err(|_| InvalidValue::Malformed.param_error())?;
-
-        let salt = Salt::new(b64_strip(salt))?;
-        let hash = Output::b64_decode(b64_strip(hash))?;
-
-        Ok(PasswordHash {
-            algorithm: ALG_ID,
-            version: None,
-            params: params.try_into()?,
-            salt: Some(salt),
-            hash: Some(hash),
-        })
-    }
-}
-
-/// Strip trailing `=` signs off a Base64 value to make a valid B64 value
-pub fn b64_strip(mut s: &str) -> &str {
-    while s.ends_with('=') {
-        s = &s[..(s.len() - 1)]
-    }
-    s
 }
