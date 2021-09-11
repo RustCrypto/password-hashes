c03e217373b824fb6534873d2c0c9de16ab7f207
Tony Arcieri <bascule@gmail.com>
2021-09-11 07:57:09 -0600

    argon2: fix handling of `p_cost` parameter
    It was not being passed correctly to `Instance`
    
    Closes #234

--- argon2/src/instance.rs
+++ argon2/src/instance.rs
@@ -99,7 +99,7 @@ impl<'a> Instance<'a> {
             passes: context.params.t_cost(),
             lane_length,
             lanes: context.params.lanes(),
-            threads: context.params.t_cost(),
+            threads: context.params.p_cost(),
             alg,
         };
 
