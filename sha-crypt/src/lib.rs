afa29a3ad6ef3555fb6811ef5618f251944ee62f
Tony Arcieri <bascule@gmail.com>
2021-09-17 20:46:25 -0600

    sha-crypt v0.3.1 (#243)

--- sha-crypt/src/lib.rs
+++ sha-crypt/src/lib.rs
@@ -33,7 +33,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/sha-crypt/0.3.0"
+    html_root_url = "https://docs.rs/sha-crypt/0.3.1"
 )]
 #![deny(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
