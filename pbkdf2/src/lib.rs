d1ab6ad3deb8bf6b9394062df45713ccb6d8610b
Tony Arcieri <bascule@gmail.com>
2022-03-18 14:21:19 -0600

    pbkdf2 v0.11.0 (#291)

--- pbkdf2/src/lib.rs
+++ pbkdf2/src/lib.rs
@@ -56,8 +56,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/pbkdf2/0.11.0-pre"
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg"
 )]
 
 #[cfg(feature = "std")]
