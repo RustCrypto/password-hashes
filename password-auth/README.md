69bdbc026abef3b240de083655ba6d1cf72b9ada
Tony Arcieri <bascule@gmail.com>
2022-11-13 18:30:51 -0700

    password-auth: initial crate
    Adds a high-level password hashing crate which is a facade over other
    crates in this repo, namely the `argon2`, `pbkdf2`, and `scrypt` crates.
    
    The crate has been designed with an extremely simple API with only
    `generate_hash` and `verify_password` functions.
    
    The API surface deliberately avoids exposing anything from other crates,
    allowing for rapid iteration towards a 1.0 release even if the crates
    it's built on are not yet 1.0.

---
+++ password-auth/README.md
@@ -0,0 +1,59 @@
+# RustCrypto: Password Authentication
+
+[![crate][crate-image]][crate-link]
+[![Docs][docs-image]][docs-link]
+[![Build Status][build-image]][build-link]
+![Apache2/MIT licensed][license-image]
+![Rust Version][rustc-image]
+[![Project Chat][chat-image]][chat-link]
+
+Password authentication library with a focus on simplicity and ease-of-use,
+with support for [Argon2], [PBKDF2], and [scrypt] password hashing algorithms.
+
+[Documentation][docs-link]
+
+## Minimum Supported Rust Version
+
+Rust **1.57** or higher.
+
+Minimum supported Rust version can be changed in the future, but it will be
+done with a minor version bump.
+
+## SemVer Policy
+
+- All on-by-default features of this library are covered by SemVer
+- MSRV is considered exempt from SemVer as noted above
+
+## License
+
+Licensed under either of:
+
+ * [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+ * [MIT license](http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+[//]: # (badges)
+
+[crate-image]: https://img.shields.io/crates/v/password-auth.svg
+[crate-link]: https://crates.io/crates/password-auth
+[docs-image]: https://docs.rs/password-auth/badge.svg
+[docs-link]: https://docs.rs/password-auth/
+[license-image]: https://img.shields.io/badge/license-Apache2.0/MIT-blue.svg
+[rustc-image]: https://img.shields.io/badge/rustc-1.57+-blue.svg
+[chat-image]: https://img.shields.io/badge/zulip-join_chat-blue.svg
+[chat-link]: https://rustcrypto.zulipchat.com/#narrow/stream/260046-password-hashes
+[build-image]: https://github.com/RustCrypto/password-hashes/workflows/password-auth/badge.svg?branch=master&event=push
+[build-link]: https://github.com/RustCrypto/password-hashes/actions?query=workflow%3Apassword-auth
+
+[//]: # (general links)
+
+[Argon2]: https://en.wikipedia.org/wiki/Argon2
+[PBKDF2]: https://en.wikipedia.org/wiki/PBKDF2
+[scrypt]: https://en.wikipedia.org/wiki/Scrypt
---
+++ password-auth/src/lib.rs
@@ -0,0 +1,155 @@
+#![no_std]
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg"
+)]
+#![warn(
+    clippy::checked_conversions,
+    clippy::integer_arithmetic,
+    clippy::panic,
+    clippy::panic_in_result_fn,
+    clippy::unwrap_used,
+    missing_docs,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
+extern crate alloc;
+#[cfg(feature = "std")]
+extern crate std;
+
+use alloc::string::{String, ToString};
+use core::fmt;
+use password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString};
+use rand_core::OsRng;
+
+#[cfg(not(any(feature = "argon2", feature = "pbkdf2", feature = "scrypt")))]
+compile_error!(
+    "please enable at least one password hash crate feature, e.g. argon2, pbkdf2, scrypt"
+);
+
+#[cfg(feature = "argon2")]
+use argon2::Argon2;
+#[cfg(feature = "pbkdf2")]
+use pbkdf2::Pbkdf2;
+#[cfg(feature = "scrypt")]
+use scrypt::Scrypt;
+
+/// Opaque error type.
+#[derive(Clone, Copy, Debug)]
+pub struct VerifyError;
+
+impl fmt::Display for VerifyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("password verification error")
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+impl std::error::Error for VerifyError {}
+
+/// Generate a password hash for the given password.
+pub fn generate_hash(password: impl AsRef<[u8]>) -> String {
+    let salt = SaltString::generate(OsRng);
+    generate_phc_hash(password.as_ref(), salt.as_ref())
+        .map(|hash| hash.to_string())
+        .expect("password hashing error")
+}
+
+/// Generate a PHC hash using the preferred algorithm.
+#[allow(unreachable_code)]
+fn generate_phc_hash<'a>(
+    password: &[u8],
+    salt: &'a str,
+) -> password_hash::Result<PasswordHash<'a>> {
+    //
+    // Algorithms below are in order of preference
+    //
+    #[cfg(feature = "argon2")]
+    return Argon2::default().hash_password(password, salt);
+
+    #[cfg(feature = "scrypt")]
+    return Scrypt.hash_password(password, salt);
+
+    #[cfg(feature = "pbkdf2")]
+    return Pbkdf2.hash_password(password, salt);
+}
+
+/// Verify the provided password against the provided password hash.
+pub fn verify_password(password: impl AsRef<[u8]>, hash: &str) -> Result<(), VerifyError> {
+    let hash = PasswordHash::new(hash).map_err(|_| VerifyError)?;
+
+    let algs: &[&dyn PasswordVerifier] = &[
+        #[cfg(feature = "argon2")]
+        &Argon2::default(),
+        #[cfg(feature = "pbkdf2")]
+        &Pbkdf2,
+        #[cfg(feature = "scrypt")]
+        &Scrypt,
+    ];
+
+    hash.verify_password(algs, password)
+        .map_err(|_| VerifyError)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{generate_hash, verify_password};
+
+    const EXAMPLE_PASSWORD: &str = "password";
+
+    #[test]
+    fn happy_path() {
+        let hash = generate_hash(EXAMPLE_PASSWORD);
+        assert!(verify_password(EXAMPLE_PASSWORD, &hash).is_ok());
+        assert!(verify_password("bogus", &hash).is_err());
+    }
+
+    #[cfg(feature = "argon2")]
+    mod argon2 {
+        use super::{verify_password, EXAMPLE_PASSWORD};
+
+        /// Argon2 hash for the string "password".
+        const EXAMPLE_HASH: &str =
+            "$argon2i$v=19$m=65536,t=1,p=1$c29tZXNhbHQAAAAAAAAAAA$+r0d29hqEB0yasKr55ZgICsQGSkl0v0kgwhd+U3wyRo";
+
+        #[test]
+        fn verify() {
+            assert!(verify_password(EXAMPLE_PASSWORD, EXAMPLE_HASH).is_ok());
+            assert!(verify_password("bogus", EXAMPLE_HASH).is_err());
+        }
+    }
+
+    #[cfg(feature = "pbkdf2")]
+    mod pdkdf2 {
+        use super::{verify_password, EXAMPLE_PASSWORD};
+
+        /// PBKDF2 hash for the string "password".
+        const EXAMPLE_HASH: &str =
+            "$pbkdf2-sha256$i=4096,l=32$c2FsdA$xeR41ZKIyEGqUw22hFxMjZYok6ABzk4RpJY4c6qYE0o";
+
+        #[test]
+        fn verify() {
+            assert!(verify_password(EXAMPLE_PASSWORD, EXAMPLE_HASH).is_ok());
+            assert!(verify_password("bogus", EXAMPLE_HASH).is_err());
+        }
+    }
+
+    #[cfg(feature = "scrypt")]
+    mod scrypt {
+        use super::{verify_password, EXAMPLE_PASSWORD};
+
+        /// scrypt hash for the string "password".
+        const EXAMPLE_HASH: &str =
+            "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E";
+
+        #[test]
+        fn verify() {
+            assert!(verify_password(EXAMPLE_PASSWORD, EXAMPLE_HASH).is_ok());
+            assert!(verify_password("bogus", EXAMPLE_HASH).is_err());
+        }
+    }
+}
