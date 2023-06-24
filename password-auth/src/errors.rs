45826c5f34d2ba5e02295045000da87be2eb7e5f
Tony Arcieri <bascule@gmail.com>
2023-06-24 14:29:13 -0600

    password-auth: replace `VerifyError` with `Error` enum (#429)
    `VerifyError` was previously the return type for the `verify_password`
    function which has two distinct error cases:
    
    - Password hash failed to parse
    - Password is invalid
    
    This commit replaces `VerifyError` with an `Error` enum which captures
    both cases.
    
    It leverages the `ParseError` type which was added in #428.

--- password-auth/src/errors.rs
+++ password-auth/src/errors.rs
@@ -1,15 +1,55 @@
 //! Error types.
 
+use alloc::string::ToString;
 use core::fmt;
 
-/// Password hash parse errors.
+/// Error type.
 #[derive(Clone, Copy, Debug)]
+pub enum Error {
+    /// Password hash parsing errors.
+    Parse(ParseError),
+
+    /// Password is invalid.
+    PasswordInvalid,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Parse(err) => write!(f, "{err}"),
+            Self::PasswordInvalid => write!(f, "password is invalid"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
+
+impl From<ParseError> for Error {
+    fn from(err: ParseError) -> Error {
+        Error::Parse(err)
+    }
+}
+
+/// Password hash parse errors.
+// This type has no public constructor and deliberately keeps
+// `password_hash::Error` out of the public API so it can evolve
+// independently (e.g. get to 1.0 faster)
+#[derive(Clone, Copy)]
 pub struct ParseError(password_hash::Error);
 
 impl ParseError {
     /// Create a new parse error.
     pub(crate) fn new(err: password_hash::Error) -> Self {
         Self(err)
+    }
+}
+
+impl fmt::Debug for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("ParseError")
+            .field(&self.0.to_string())
+            .finish()
     }
 }
 
@@ -21,16 +61,3 @@ impl fmt::Display for ParseError {
 
 #[cfg(feature = "std")]
 impl std::error::Error for ParseError {}
-
-/// Password hash verification errors.
-#[derive(Clone, Copy, Debug)]
-pub struct VerifyError;
-
-impl fmt::Display for VerifyError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("password verification error")
-    }
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for VerifyError {}
