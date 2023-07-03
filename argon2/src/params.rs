524de64fe7da9a940a3532d362f87b92a74c0a5f
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2023-07-03 18:02:44 +0200

    Add tarcieri's change
    Co-authored-by: Tony Arcieri <bascule@gmail.com>

--- argon2/src/params.rs
+++ argon2/src/params.rs
@@ -232,13 +232,11 @@ macro_rules! param_buf {
                 Ok(Self { bytes, len })
             }
 
-            /// Just like [Default::default], but const.
-            pub const fn default_const() -> Self {
-                Self {
-                    bytes: [0u8; Self::MAX_LEN],
-                    len: 0,
-                }
-            }
+            /// Empty value.
+            pub const EMPTY: Self = Self {
+                bytes: [0u8; Self::MAX_LEN],
+                len: 0,
+            };
 
             #[doc = "Decode"]
             #[doc = $name]
