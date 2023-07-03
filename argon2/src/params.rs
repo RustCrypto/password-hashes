8ac350bde62795b3deffa8951b3807c1a148550e
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2023-07-03 18:27:53 +0200

    Revert "Even more const"
    This reverts commit 383be359936913010b62763aefff00d95c6e28c4.

--- argon2/src/params.rs
+++ argon2/src/params.rs
@@ -145,7 +145,7 @@ impl Params {
     /// On top of that, this field is not longer part of the Argon2 standard
     /// (see: <https://github.com/P-H-C/phc-winner-argon2/pull/173>), and should
     /// not be used for any non-legacy work.
-    pub const fn keyid(&self) -> &[u8] {
+    pub fn keyid(&self) -> &[u8] {
         self.keyid.as_bytes()
     }
 
@@ -156,7 +156,7 @@ impl Params {
     /// This field is not longer part of the argon2 standard
     /// (see: <https://github.com/P-H-C/phc-winner-argon2/pull/173>), and should
     /// not be used for any non-legacy work.
-    pub const fn data(&self) -> &[u8] {
+    pub fn data(&self) -> &[u8] {
         self.data.as_bytes()
     }
 
@@ -246,8 +246,8 @@ macro_rules! param_buf {
             }
 
             /// Borrow the inner value as a byte slice.
-            pub const fn as_bytes(&self) -> &[u8] {
-                self.bytes.split_at(self.len).0
+            pub fn as_bytes(&self) -> &[u8] {
+                &self.bytes[..self.len]
             }
 
             /// Get the length in bytes.
