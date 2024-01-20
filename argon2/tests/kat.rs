RustCrypto/password-hashes

commit e8f01946ddc5c3780156ddf50f6f6f62cd49ac36

argon2: refactor crate (#247)
- Change ParamsBuilder to make it more ergonomic
- Remove unnecessary suppressions of dead code warnings
- Fix wrong docs for m_cost
- Fix wrong doc for Block
- Merge `Argon2` and `Instance` structs, remove Memory
- Remove parallel feature (to be re-added)

argon2/src/block.rs
@@ -1,17 +1,109 @@
 //! Argon2 memory block functions
 
 use core::{
+    convert::{AsMut, AsRef},
     num::Wrapping,
-    ops::{BitXor, BitXorAssign, Index, IndexMut},
-    slice,
+    ops::{BitXor, BitXorAssign},
 };
 
 #[cfg(feature = "zeroize")]
 use zeroize::Zeroize;
 
-/// Structure for the (1KB) memory block implemented as 128 64-bit words.
+const TRUNC: u64 = u32::MAX as u64;
+
+macro_rules! permutate_step {
+    ($a:expr, $b:expr, $c:expr, $d:expr) => {
+        $a =
+            (Wrapping($a) + Wrapping($b) + (Wrapping(2) * Wrapping(($a & TRUNC) * ($b & TRUNC)))).0;
+        $d = ($d ^ $a).rotate_right(32);
+        $c =
+            (Wrapping($c) + Wrapping($d) + (Wrapping(2) * Wrapping(($c & TRUNC) * ($d & TRUNC)))).0;
+        $b = ($b ^ $c).rotate_right(24);
+
+        $a =
+            (Wrapping($a) + Wrapping($b) + (Wrapping(2) * Wrapping(($a & TRUNC) * ($b & TRUNC)))).0;
+        $d = ($d ^ $a).rotate_right(16);
+        $c =
+            (Wrapping($c) + Wrapping($d) + (Wrapping(2) * Wrapping(($c & TRUNC) * ($d & TRUNC)))).0;
+        $b = ($b ^ $c).rotate_right(63);
+    };
+}
+
+macro_rules! permutate {
+    (
+        $v0:expr, $v1:expr, $v2:expr, $v3:expr,
+        $v4:expr, $v5:expr, $v6:expr, $v7:expr,
+        $v8:expr, $v9:expr, $v10:expr, $v11:expr,
+        $v12:expr, $v13:expr, $v14:expr, $v15:expr,
+    ) => {
+        permutate_step!($v0, $v4, $v8, $v12);
+        permutate_step!($v1, $v5, $v9, $v13);
+        permutate_step!($v2, $v6, $v10, $v14);
+        permutate_step!($v3, $v7, $v11, $v15);
+        permutate_step!($v0, $v5, $v10, $v15);
+        permutate_step!($v1, $v6, $v11, $v12);
+        permutate_step!($v2, $v7, $v8, $v13);
+        permutate_step!($v3, $v4, $v9, $v14);
+    };
+}
+
+/// Structure for the (1 KiB) memory block implemented as 128 64-bit words.
 #[derive(Copy, Clone, Debug)]
+#[repr(align(64))]
 pub struct Block([u64; Self::SIZE / 8]);
+
+impl Block {
+    /// Memory block size in bytes
+    pub const SIZE: usize = 1024;
+
+    pub(crate) fn as_bytes(&self) -> &[u8; Self::SIZE] {
+        let ptr = self.0.as_ptr() as *const u8;
+        let slice = unsafe { core::slice::from_raw_parts(ptr, Self::SIZE) };
+        slice.try_into().unwrap()
+    }
+
+    pub(crate) fn as_mut_bytes(&mut self) -> &mut [u8; Self::SIZE] {
+        let ptr = self.0.as_mut_ptr() as *mut u8;
+        let slice = unsafe { core::slice::from_raw_parts_mut(ptr, Self::SIZE) };
+        slice.try_into().unwrap()
+    }
+
+    pub(crate) fn compress(rhs: &Self, lhs: &Self) -> Self {
+        let r = *rhs ^ lhs;
+
+        // Apply permutations rowwise
+        let mut q = r;
+        for chunk in q.0.chunks_exact_mut(16) {
+            #[rustfmt::skip]
+            permutate!(
+                chunk[0], chunk[1], chunk[2], chunk[3],
+                chunk[4], chunk[5], chunk[6], chunk[7],
+                chunk[8], chunk[9], chunk[10], chunk[11],
+                chunk[12], chunk[13], chunk[14], chunk[15],
+            );
+        }
+
+        // Apply permutations columnwise
+        for i in 0..8 {
+            let b = i * 2;
+
+            #[rustfmt::skip]
+            permutate!(
+                q.0[b], q.0[b + 1],
+                q.0[b + 16], q.0[b + 17],
+                q.0[b + 32], q.0[b + 33],
+                q.0[b + 48], q.0[b + 49],
+                q.0[b + 64], q.0[b + 65],
+                q.0[b + 80], q.0[b + 81],
+                q.0[b + 96], q.0[b + 97],
+                q.0[b + 112], q.0[b + 113],
+            );
+        }
+
+        q ^= &r;
+        q
+    }
+}
 
 impl Default for Block {
     fn default() -> Self {
@@ -19,159 +111,32 @@ impl Default for Block {
     }
 }
 
-impl Block {
-    /// Memory block size in bytes
-    pub const SIZE: usize = 1024;
-
-    /// Load a block from a block-sized byte slice
-    pub(crate) fn load(&mut self, input: &[u8]) {
-        debug_assert_eq!(input.len(), Block::SIZE);
-
-        for (i, chunk) in input.chunks(8).enumerate() {
-            self[i] = u64::from_le_bytes(chunk.try_into().unwrap());
-        }
-    }
-
-    /// Iterate over the `u64` values contained in this block
-    pub(crate) fn iter(&self) -> slice::Iter<'_, u64> {
-        self.0.iter()
-    }
-
-    /// Iterate mutably over the `u64` values contained in this block
-    pub(crate) fn iter_mut(&mut self) -> slice::IterMut<'_, u64> {
-        self.0.iter_mut()
-    }
-
-    /// Function fills a new memory block and optionally XORs the old block over the new one.
-    // TODO(tarcieri): optimized implementation (i.e. from opt.c instead of ref.c)
-    pub(crate) fn fill_block(&mut self, prev_block: Block, ref_block: Block, with_xor: bool) {
-        let mut block_r = ref_block ^ prev_block;
-        let mut block_tmp = block_r;
-
-        // Now block_r = ref_block + prev_block and block_tmp = ref_block + prev_block
-        if with_xor {
-            // Saving the next block contents for XOR over
-            block_tmp ^= *self;
-            // Now block_r = ref_block + prev_block and
-            // block_tmp = ref_block + prev_block + next_block
-        }
-
-        /// Note: designed by the Lyra PHC team
-        fn blake2_mult(x: u64, y: u64) -> u64 {
-            let m = 0xFFFFFFFF;
-            let xy = Wrapping((x & m) * (y & m)) * Wrapping(2);
-            (Wrapping(x) + Wrapping(y) + xy).0
-        }
-
-        /// Blake2 round function
-        macro_rules! blake2_round {
-            (
-                $v0:expr, $v1:expr, $v2:expr, $v3:expr, $v4:expr, $v5:expr, $v6:expr, $v7:expr,
-                $v8:expr, $v9:expr, $v10:expr, $v11:expr, $v12:expr, $v13:expr, $v14:expr, $v15:expr
-            ) => {
-                blake2_inner!($v0, $v4, $v8, $v12);
-                blake2_inner!($v1, $v5, $v9, $v13);
-                blake2_inner!($v2, $v6, $v10, $v14);
-                blake2_inner!($v3, $v7, $v11, $v15);
-                blake2_inner!($v0, $v5, $v10, $v15);
-                blake2_inner!($v1, $v6, $v11, $v12);
-                blake2_inner!($v2, $v7, $v8, $v13);
-                blake2_inner!($v3, $v4, $v9, $v14);
-            };
-        }
-
-        macro_rules! blake2_inner {
-            ($a:expr, $b:expr, $c:expr, $d:expr) => {
-                $a = blake2_mult($a, $b);
-                $d = ($d ^ $a).rotate_right(32);
-                $c = blake2_mult($c, $d);
-                $b = ($b ^ $c).rotate_right(24);
-                $a = blake2_mult($a, $b);
-                $d = ($d ^ $a).rotate_right(16);
-                $c = blake2_mult($c, $d);
-                $b = ($b ^ $c).rotate_right(63);
-            };
-        }
-
-        // Apply Blake2 on columns of 64-bit words: (0, 1, ..., 15), then
-        // (16, 17, ..31)... finally (112, 113, ...127)
-        for i in 0..8 {
-            blake2_round!(
-                block_r[16 * i],
-                block_r[16 * i + 1],
-                block_r[16 * i + 2],
-                block_r[16 * i + 3],
-                block_r[16 * i + 4],
-                block_r[16 * i + 5],
-                block_r[16 * i + 6],
-                block_r[16 * i + 7],
-                block_r[16 * i + 8],
-                block_r[16 * i + 9],
-                block_r[16 * i + 10],
-                block_r[16 * i + 11],
-                block_r[16 * i + 12],
-                block_r[16 * i + 13],
-                block_r[16 * i + 14],
-                block_r[16 * i + 15]
-            );
-        }
-
-        // Apply Blake2 on rows of 64-bit words: (0, 1, 16, 17, ...112, 113), then
-        // (2, 3, 18, 19, ..., 114, 115).. finally (14, 15, 30, 31, ..., 126, 127)
-        for i in 0..8 {
-            blake2_round!(
-                block_r[2 * i],
-                block_r[2 * i + 1],
-                block_r[2 * i + 16],
-                block_r[2 * i + 17],
-                block_r[2 * i + 32],
-                block_r[2 * i + 33],
-                block_r[2 * i + 48],
-                block_r[2 * i + 49],
-                block_r[2 * i + 64],
-                block_r[2 * i + 65],
-                block_r[2 * i + 80],
-                block_r[2 * i + 81],
-                block_r[2 * i + 96],
-                block_r[2 * i + 97],
-                block_r[2 * i + 112],
-                block_r[2 * i + 113]
-            );
-        }
-
-        *self = block_tmp ^ block_r;
+impl AsRef<[u64]> for Block {
+    fn as_ref(&self) -> &[u64] {
+        &self.0
     }
 }
 
-impl BitXor for Block {
-    type Output = Self;
-
-    fn bitxor(self, rhs: Self) -> Self::Output {
-        let mut res = self;
-        res ^= rhs;
-        res
+impl AsMut<[u64]> for Block {
+    fn as_mut(&mut self) -> &mut [u64] {
+        &mut self.0
     }
 }
 
-impl BitXorAssign for Block {
-    fn bitxor_assign(&mut self, rhs: Self) {
-        for (a, b) in self.iter_mut().zip(rhs.iter()) {
-            *a ^= *b;
+impl BitXor<&Block> for Block {
+    type Output = Block;
+
+    fn bitxor(mut self, rhs: &Block) -> Self::Output {
+        self ^= rhs;
+        self
+    }
+}
+
+impl BitXorAssign<&Block> for Block {
+    fn bitxor_assign(&mut self, rhs: &Block) {
+        for (dst, src) in self.0.iter_mut().zip(rhs.0.iter().copied()) {
+            *dst ^= src;
         }
-    }
-}
-
-impl Index<usize> for Block {
-    type Output = u64;
-
-    fn index(&self, index: usize) -> &u64 {
-        &self.0[index]
-    }
-}
-
-impl IndexMut<usize> for Block {
-    fn index_mut(&mut self, index: usize) -> &mut u64 {
-        &mut self.0[index]
     }
 }
 

argon2/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 // TODO(tarcieri): safe parallel implementation
 // See: https://github.com/RustCrypto/password-hashes/issues/154
-#![cfg_attr(not(feature = "parallel"), forbid(unsafe_code))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 #![doc(
@@ -68,16 +67,15 @@ extern crate std;
 mod algorithm;
 mod block;
 mod error;
-mod instance;
-mod memory;
 mod params;
+mod variable_hash;
 mod version;
 
 pub use crate::{
     algorithm::Algorithm,
     block::Block,
     error::{Error, Result},
-    params::{Params, ParamsBuilder},
+    params::{AssociatedData, KeyId, Params, ParamsBuilder},
     version::Version,
 };
 
@@ -88,14 +86,17 @@ pub use {
     password_hash::{self, PasswordHash, PasswordHasher, PasswordVerifier},
 };
 
-use crate::{
-    instance::Instance,
-    memory::{Memory, SYNC_POINTS},
+use crate::variable_hash::blake2b_long;
+use blake2::{
+    digest::{self, Output},
+    Blake2b512, Digest,
 };
-use blake2::{digest::Output, Blake2b512, Digest};
 
 #[cfg(all(feature = "alloc", feature = "password-hash"))]
 use password_hash::{Decimal, Ident, ParamsString, Salt};
+
+#[cfg(feature = "zeroize")]
+use zeroize::Zeroize;
 
 /// Maximum password length in bytes.
 pub const MAX_PWD_LEN: usize = 0xFFFFFFFF;
@@ -111,6 +112,12 @@ pub const RECOMMENDED_SALT_LEN: usize = 16;
 
 /// Maximum secret key length in bytes.
 pub const MAX_SECRET_LEN: usize = 0xFFFFFFFF;
+
+/// Number of synchronization points between lanes per pass
+pub(crate) const SYNC_POINTS: usize = 4;
+
+/// To generate reference block positions
+const ADDRESSES_IN_BLOCK: usize = 128;
 
 /// Argon2 context.
 ///
@@ -204,31 +211,200 @@ impl<'key> Argon2<'key> {
             return Err(Error::OutputTooLong);
         }
 
-        if pwd.len() > MAX_PWD_LEN {
-            return Err(Error::PwdTooLong);
-        }
-
-        // Validate salt (required param)
-        if salt.len() < MIN_SALT_LEN {
-            return Err(Error::SaltTooShort);
-        }
-
-        if salt.len() > MAX_SALT_LEN {
-            return Err(Error::SaltTooLong);
-        }
+        Self::verify_inputs(pwd, salt)?;
 
         // Hashing all inputs
         let initial_hash = self.initial_hash(pwd, salt, out);
 
-        let segment_length = self.params.segment_length();
+        self.fill_blocks(memory_blocks.as_mut(), initial_hash)?;
+        self.finalize(memory_blocks.as_mut(), out)
+    }
+
+    /// Use a password and associated parameters only to fill the given memory blocks.
+    ///
+    /// This method omits the calculation of a hash and can be used when only the
+    /// filled memory is required. It is not necessary to call this method
+    /// before calling any of the hashing functions.
+    pub fn fill_memory(
+        &self,
+        pwd: &[u8],
+        salt: &[u8],
+        mut memory_blocks: impl AsMut<[Block]>,
+    ) -> Result<()> {
+        Self::verify_inputs(pwd, salt)?;
+
+        let initial_hash = self.initial_hash(pwd, salt, &[]);
+
+        self.fill_blocks(memory_blocks.as_mut(), initial_hash)
+    }
+
+    #[allow(unused_mut)]
+    fn fill_blocks(
+        &self,
+        memory_blocks: &mut [Block],
+        mut initial_hash: digest::Output<Blake2b512>,
+    ) -> Result<()> {
         let block_count = self.params.block_count();
         let memory_blocks = memory_blocks
-            .as_mut()
             .get_mut(..block_count)
             .ok_or(Error::MemoryTooLittle)?;
 
-        let memory = Memory::new(memory_blocks, segment_length);
-        Instance::hash(self, self.algorithm, initial_hash, memory, out)
+        let segment_length = self.params.segment_length();
+        let iterations = usize::try_from(self.params.t_cost()).unwrap();
+        let lane_length = self.params.lane_length();
+        let lanes = self.params.lanes();
+
+        // Initialize the first two blocks in each lane
+        for (l, lane) in memory_blocks.chunks_exact_mut(lane_length).enumerate() {
+            for (i, block) in lane[..2].iter_mut().enumerate() {
+                let i = u32::try_from(i).unwrap();
+                let l = u32::try_from(l).unwrap();
+
+                let inputs = &[
+                    initial_hash.as_ref(),
+                    &i.to_le_bytes()[..],
+                    &l.to_le_bytes()[..],
+                ];
+
+                blake2b_long(inputs, block.as_mut_bytes()).unwrap();
+            }
+        }
+
+        #[cfg(feature = "zeroize")]
+        initial_hash.zeroize();
+
+        // Run passes on blocks
+        for pass in 0..iterations {
+            for slice in 0..SYNC_POINTS {
+                let data_independent_addressing = self.algorithm == Algorithm::Argon2i
+                    || (self.algorithm == Algorithm::Argon2id
+                        && pass == 0
+                        && slice < SYNC_POINTS / 2);
+
+                for lane in 0..lanes {
+                    let mut address_block = Block::default();
+                    let mut input_block = Block::default();
+                    let zero_block = Block::default();
+
+                    if data_independent_addressing {
+                        (&mut input_block.as_mut()[..6]).copy_from_slice(&[
+                            pass as u64,
+                            lane as u64,
+                            slice as u64,
+                            memory_blocks.len() as u64,
+                            iterations as u64,
+                            self.algorithm as u64,
+                        ]);
+                    }
+
+                    let first_block = if pass == 0 && slice == 0 {
+                        if data_independent_addressing {
+                            // Generate first set of addresses
+                            Self::update_address_block(
+                                &mut address_block,
+                                &mut input_block,
+                                &zero_block,
+                            );
+                        }
+
+                        // The first two blocks of each lane are already initialized
+                        2
+                    } else {
+                        0
+                    };
+
+                    let mut cur_index = lane * lane_length + slice * segment_length + first_block;
+                    let mut prev_index = if slice == 0 && first_block == 0 {
+                        // Last block in current lane
+                        cur_index + lane_length - 1
+                    } else {
+                        // Previous block
+                        cur_index - 1
+                    };
+
+                    // Fill blocks in the segment
+                    for block in first_block..segment_length {
+                        // Extract entropy
+                        let rand = if data_independent_addressing {
+                            let addres_index = block % ADDRESSES_IN_BLOCK;
+
+                            if addres_index == 0 {
+                                Self::update_address_block(
+                                    &mut address_block,
+                                    &mut input_block,
+                                    &zero_block,
+                                );
+                            }
+
+                            address_block.as_ref()[addres_index]
+                        } else {
+                            memory_blocks[prev_index].as_ref()[0]
+                        };
+
+                        // Calculate source block index for compress function
+                        let ref_lane = if pass == 0 && slice == 0 {
+                            // Cannot reference other lanes yet
+                            lane
+                        } else {
+                            (rand >> 32) as usize % lanes as usize
+                        };
+
+                        let reference_area_size = if pass == 0 {
+                            // First pass
+                            if slice == 0 {
+                                // First slice
+                                block - 1 // all but the previous
+                            } else if ref_lane == lane {
+                                // The same lane => add current segment
+                                slice * segment_length + block - 1
+                            } else {
+                                slice * segment_length - if block == 0 { 1 } else { 0 }
+                            }
+                        } else {
+                            // Second pass
+                            if ref_lane == lane {
+                                lane_length - segment_length + block - 1
+                            } else {
+                                lane_length - segment_length - if block == 0 { 1 } else { 0 }
+                            }
+                        };
+
+                        // 1.2.4. Mapping rand to 0..<reference_area_size-1> and produce
+                        // relative position
+                        let mut map = rand & 0xFFFFFFFF;
+                        map = (map * map) >> 32;
+                        let relative_position = reference_area_size
+                            - 1
+                            - ((reference_area_size as u64 * map) >> 32) as usize;
+
+                        // 1.2.5 Computing starting position
+                        let start_position = if pass != 0 && slice != SYNC_POINTS - 1 {
+                            (slice + 1) * segment_length
+                        } else {
+                            0
+                        };
+
+                        let lane_index = (start_position + relative_position) % lane_length;
+                        let ref_index = ref_lane * lane_length + lane_index;
+
+                        // Calculate new block
+                        let result =
+                            Block::compress(&memory_blocks[prev_index], &memory_blocks[ref_index]);
+
+                        if self.version == Version::V0x10 || pass == 0 {
+                            memory_blocks[cur_index] = result;
+                        } else {
+                            memory_blocks[cur_index] ^= &result;
+                        };
+
+                        prev_index = cur_index;
+                        cur_index += 1;
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
 
     /// Get default configured [`Params`].
@@ -236,10 +412,39 @@ impl<'key> Argon2<'key> {
         &self.params
     }
 
+    fn finalize(&self, memory_blocks: &[Block], out: &mut [u8]) -> Result<()> {
+        let lane_length = self.params.lane_length();
+
+        let mut blockhash = memory_blocks[lane_length - 1];
+
+        // XOR the last blocks
+        for l in 1..self.params.lanes() {
+            let last_block_in_lane = l * lane_length + (lane_length - 1);
+            blockhash ^= &memory_blocks[last_block_in_lane];
+        }
+
+        blake2b_long(&[blockhash.as_bytes()], out)?;
+
+        #[cfg(feature = "zeroize")]
+        blockhash.zeroize();
+
+        Ok(())
+    }
+
+    fn update_address_block(
+        address_block: &mut Block,
+        input_block: &mut Block,
+        zero_block: &Block,
+    ) {
+        input_block.as_mut()[6] += 1;
+        *address_block = Block::compress(zero_block, input_block);
+        *address_block = Block::compress(zero_block, address_block);
+    }
+
     /// Hashes all the inputs into `blockhash[PREHASH_DIGEST_LEN]`.
-    pub(crate) fn initial_hash(&self, pwd: &[u8], salt: &[u8], out: &[u8]) -> Output<Blake2b512> {
+    fn initial_hash(&self, pwd: &[u8], salt: &[u8], out: &[u8]) -> Output<Blake2b512> {
         let mut digest = Blake2b512::new();
-        digest.update(&self.params.lanes().to_le_bytes());
+        digest.update(&self.params.p_cost().to_le_bytes());
         digest.update(&(out.len() as u32).to_le_bytes());
         digest.update(&self.params.m_cost().to_le_bytes());
         digest.update(&self.params.t_cost().to_le_bytes());
@@ -260,6 +465,23 @@ impl<'key> Argon2<'key> {
         digest.update(&(self.params.data().len() as u32).to_le_bytes());
         digest.update(self.params.data());
         digest.finalize()
+    }
+
+    fn verify_inputs(pwd: &[u8], salt: &[u8]) -> Result<()> {
+        if pwd.len() > MAX_PWD_LEN {
+            return Err(Error::PwdTooLong);
+        }
+
+        // Validate salt (required param)
+        if salt.len() < MIN_SALT_LEN {
+            return Err(Error::SaltTooShort);
+        }
+
+        if salt.len() > MAX_SALT_LEN {
+            return Err(Error::SaltTooLong);
+        }
+
+        Ok(())
     }
 }
 
@@ -389,7 +611,7 @@ mod tests {
                     .get(param)
                     .and_then(|p| p.decimal().ok())
                     .unwrap(),
-                value
+                value,
             );
         }
     }

commit d2d91bdb7c31014c23f42e0b0e3e4e28b12620a3

argon2: fix big endian support
This was broken in #247, which added unsafe pointer casts to access the
contents of a block, which only works on little endian targets.

This reverts back to something closer to the previous code, which used
`u64::{from_le_bytes, to_le_bytes}` instead.

It also adds cross tests for PPC32 to spot future regressions.

.github/workflows/argon2.yml
@@ -48,16 +48,48 @@ jobs:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        rust:
-          - 1.65.0 # MSRV
-          - stable
+        include:
+          # 32-bit Linux
+          - target: i686-unknown-linux-gnu
+            rust: 1.65.0 # MSRV
+            deps: sudo apt update && sudo apt install gcc-multilib
+          - target: i686-unknown-linux-gnu
+            rust: stable
+            deps: sudo apt update && sudo apt install gcc-multilib
+
+          # 64-bit Linux
+          - target: x86_64-unknown-linux-gnu
+            rust: 1.65.0 # MSRV
+          - target: x86_64-unknown-linux-gnu
+            rust: stable
     steps:
       - uses: actions/checkout@v4
       - uses: RustCrypto/actions/cargo-cache@master
       - uses: dtolnay/rust-toolchain@master
         with:
           toolchain: ${{ matrix.rust }}
+          targets: ${{ matrix.target }}
+      - run: ${{ matrix.deps }}
       - run: cargo test --no-default-features
       - run: cargo test --no-default-features --features password-hash
       - run: cargo test
       - run: cargo test --all-features
+
+  cross:
+    strategy:
+      matrix:
+        include:
+          - target: powerpc-unknown-linux-gnu
+            rust: 1.65.0 # MSRV
+          - target: powerpc-unknown-linux-gnu
+            rust: stable
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - run: ${{ matrix.deps }}
+      - uses: dtolnay/rust-toolchain@master
+        with:
+          toolchain: ${{ matrix.rust }}
+          targets: ${{ matrix.target }}
+      - uses: RustCrypto/actions/cross-install@master
+      - run: cross test --release --target ${{ matrix.target }} --all-features

argon2/src/block.rs
@@ -4,6 +4,7 @@ use core::{
     convert::{AsMut, AsRef},
     num::Wrapping,
     ops::{BitXor, BitXorAssign},
+    slice,
 };
 
 #[cfg(feature = "zeroize")]
@@ -58,12 +59,18 @@ impl Block {
         Self([0u64; Self::SIZE / 8])
     }
 
-    pub(crate) fn as_bytes(&self) -> &[u8; Self::SIZE] {
-        unsafe { &*(self.0.as_ptr() as *const [u8; Self::SIZE]) }
+    /// Load a block from a block-sized byte slice
+    #[inline(always)]
+    pub(crate) fn load(&mut self, input: &[u8; Block::SIZE]) {
+        for (i, chunk) in input.chunks(8).enumerate() {
+            self.0[i] = u64::from_le_bytes(chunk.try_into().expect("should be 8 bytes"));
+        }
     }
 
-    pub(crate) fn as_mut_bytes(&mut self) -> &mut [u8; Self::SIZE] {
-        unsafe { &mut *(self.0.as_mut_ptr() as *mut [u8; Self::SIZE]) }
+    /// Iterate over the `u64` values contained in this block
+    #[inline(always)]
+    pub(crate) fn iter(&self) -> slice::Iter<'_, u64> {
+        self.0.iter()
     }
 
     /// NOTE: do not call this directly. It should only be called via

argon2/src/lib.rs
@@ -305,13 +305,17 @@ impl<'key> Argon2<'key> {
                 let i = i as u32;
                 let l = l as u32;
 
+                // Make the first and second block in each lane as G(H0||0||i) or
+                // G(H0||1||i)
                 let inputs = &[
                     initial_hash.as_ref(),
                     &i.to_le_bytes()[..],
                     &l.to_le_bytes()[..],
                 ];
 
-                blake2b_long(inputs, block.as_mut_bytes())?;
+                let mut hash = [0u8; Block::SIZE];
+                blake2b_long(inputs, &mut hash)?;
+                block.load(&hash);
             }
         }
 
@@ -485,10 +489,20 @@ impl<'key> Argon2<'key> {
             blockhash ^= &memory_blocks[last_block_in_lane];
         }
 
-        blake2b_long(&[blockhash.as_bytes()], out)?;
+        // Hash the result
+        let mut blockhash_bytes = [0u8; Block::SIZE];
+
+        for (chunk, v) in blockhash_bytes.chunks_mut(8).zip(blockhash.iter()) {
+            chunk.copy_from_slice(&v.to_le_bytes())
+        }
+
+        blake2b_long(&[&blockhash_bytes], out)?;
 
         #[cfg(feature = "zeroize")]
-        blockhash.zeroize();
+        {
+            blockhash.zeroize();
+            blockhash_bytes.zeroize();
+        }
 
         Ok(())
     }

argon2/tests/kat.rs
@@ -307,11 +307,14 @@ fn salt_bad_length() {
     let ret = ctx.hash_password_into(b"password", &too_short_salt, &mut out);
     assert_eq!(ret, Err(Error::SaltTooShort));
 
-    // 4 GiB of RAM seems big, but as long as we ask for a zero-initialized vector
-    // optimizations kicks in an nothing is really allocated
-    let too_long_salt = vec![0u8; argon2::MAX_SALT_LEN + 1];
-    let ret = ctx.hash_password_into(b"password", &too_long_salt, &mut out);
-    assert_eq!(ret, Err(Error::SaltTooLong));
+    #[cfg(target_pointer_width = "64")] // MAX_SALT_LEN + 1 is too big for 32-bit targets
+    {
+        // 4 GiB of RAM seems big, but as long as we ask for a zero-initialized vector
+        // optimizations kicks in an nothing is really allocated
+        let too_long_salt = vec![0u8; argon2::MAX_SALT_LEN + 1];
+        let ret = ctx.hash_password_into(b"password", &too_long_salt, &mut out);
+        assert_eq!(ret, Err(Error::SaltTooLong));
+    }
 }
 
 #[test]
@@ -321,11 +324,14 @@ fn output_bad_length() {
     let ret = ctx.hash_password_into(b"password", b"diffsalt", &mut out);
     assert_eq!(ret, Err(Error::OutputTooShort));
 
-    // 4 GiB of RAM seems big, but as long as we ask for a zero-initialized vector
-    // optimizations kicks in an nothing is really allocated
-    let mut out = vec![0u8; Params::MAX_OUTPUT_LEN + 1];
-    let ret = ctx.hash_password_into(b"password", b"diffsalt", &mut out);
-    assert_eq!(ret, Err(Error::OutputTooLong));
+    #[cfg(target_pointer_width = "64")] // MAX_SALT_LEN + 1 is too big for 32-bit targets
+    {
+        // 4 GiB of RAM seems big, but as long as we ask for a zero-initialized vector
+        // optimizations kicks in an nothing is really allocated
+        let mut out = vec![0u8; Params::MAX_OUTPUT_LEN + 1];
+        let ret = ctx.hash_password_into(b"password", b"diffsalt", &mut out);
+        assert_eq!(ret, Err(Error::OutputTooLong));
+    }
 }
 
 // =======================================