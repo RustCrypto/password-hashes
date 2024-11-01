9d2d677f64f2a341cee831124c2e01353fe87f42
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2024-11-01 15:54:47 +0300

    Change param rnges

--- fuzz/fuzz_targets/scrypt.rs
+++ fuzz/fuzz_targets/scrypt.rs
@@ -12,8 +12,8 @@ pub struct ScryptRandParams(pub scrypt::Params);
 impl<'a> Arbitrary<'a> for ScryptRandParams {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
         let log_n = u.int_in_range(0..=15)?;
-        let r = u.int_in_range(1..=32)?;
-        let p = u.int_in_range(1..=16)?;
+        let r = u.int_in_range(1..=16)?;
+        let p = u.int_in_range(1..=8)?;
         let len = u.int_in_range(10..=64)?;
 
         let params = scrypt::Params::new(log_n, r, p, len).unwrap();
