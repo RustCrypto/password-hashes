7c932c88ceb45c508db90f1c4848f00038fdafe7
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2024-11-01 15:43:11 +0300

    fuzz: fix scrypt

--- fuzz/fuzz_targets/scrypt.rs
+++ fuzz/fuzz_targets/scrypt.rs
@@ -24,14 +24,19 @@ impl<'a> Arbitrary<'a> for ScryptRandParams {
 fuzz_target!(|data: (&[u8], &[u8], ScryptRandParams)| {
     let (password, salt, ScryptRandParams(params)) = data;
 
+    if password.len() > 64 {
+        return;
+    }
+
+    if salt.len() < Salt::MIN_LENGTH || salt.len() > (6 * Salt::MAX_LENGTH) / 8 {
+        return;
+    }
+
     // Check direct hashing
     let mut result = [0u8; 64];
     scrypt(password, salt, &params, &mut result).unwrap();
 
     // Check PHC hashing
-    if salt.len() < Salt::MIN_LENGTH {
-        return;
-    }
     let salt_string = SaltString::encode_b64(salt).unwrap();
     let phc_hash = Scrypt
         .hash_password_customized(
