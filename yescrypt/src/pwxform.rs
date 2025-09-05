4a101f5b64ad7ab35c5bce7f3d611952c526775d
Tony Arcieri <bascule@gmail.com>
2025-09-05 10:23:47 -0600

    yescrypt: pwxform cleanups
    - Define and use constants from the C reference implementation
    - Replace wrapping arithmetic with standard debug-checked operators

--- yescrypt/src/pwxform.rs
+++ yescrypt/src/pwxform.rs
@@ -7,6 +7,17 @@ use crate::{
 use alloc::vec::Vec;
 use core::ptr;
 
+// These are tunable, but they must meet certain constraints.
+const PWXSIMPLE: usize = 2;
+const PWXGATHER: usize = 4;
+const PWXROUNDS: usize = 6;
+const SWIDTH: usize = 8;
+
+// Derived values.  Not tunable on their own.
+const PWXBYTES: usize = PWXGATHER * PWXSIMPLE * 8;
+const PWXWORDS: usize = PWXBYTES / size_of::<u32>();
+const SMASK: usize = ((1 << SWIDTH) - 1) * PWXSIMPLE * 8;
+
 /// Parallel wide transformation (pwxform) context.
 #[derive(Copy, Clone)]
 pub(crate) struct PwxformCtx {
@@ -47,81 +58,62 @@ impl PwxformCtx {
 
         // Convert 128-byte blocks to PWXbytes blocks
         // 1: r_1 <-- 128r / PWXbytes
-        let r1 = 128usize.wrapping_mul(r).wrapping_div(4 * 2 * 8);
+        let r1 = (128 * r) / PWXBYTES;
 
         // 2: X <-- B'_{r_1 - 1}
-        blkcpy(
-            x.as_mut_ptr(),
-            b.add(
-                r1.wrapping_sub(1usize)
-                    .wrapping_mul((4usize * 2 * 8).wrapping_div(size_of::<u32>())),
-            ),
-            (4usize * 2 * 8).wrapping_div(size_of::<u32>()),
-        );
+        blkcpy(x.as_mut_ptr(), b.add((r1 - 1) * PWXWORDS), PWXWORDS);
 
         // 3: for i = 0 to r_1 - 1 do
         for i in 0..r1 {
             // 4: if r_1 > 1
             if r1 > 1 {
                 // 5: X <-- X xor B'_i
-                blkxor(
-                    x.as_mut_ptr(),
-                    b.add(i.wrapping_mul((4usize * 2 * 8).wrapping_div(size_of::<u32>()))),
-                    (4usize * 2 * 8).wrapping_div(size_of::<u32>()),
-                );
+                blkxor(x.as_mut_ptr(), b.add(i * PWXWORDS), PWXWORDS);
             }
 
             // 7: X <-- pwxform(X)
             self.pwxform(&mut x);
 
             // 8: B'_i <-- X
-            blkcpy(
-                b.add(i.wrapping_mul((4usize * 2 * 8).wrapping_div(size_of::<u32>()))),
-                x.as_mut_ptr(),
-                (4usize * 2 * 8).wrapping_div(size_of::<u32>()),
-            );
+            blkcpy(b.add(i * PWXWORDS), x.as_mut_ptr(), PWXWORDS);
         }
 
         // 10: i <-- floor((r_1 - 1) * PWXbytes / 64)
-        let i = r1.wrapping_sub(1).wrapping_mul(4 * 2 * 8).wrapping_div(64);
+        let i = (r1 - 1) * PWXBYTES / 64;
 
         // 11: B_i <-- H(B_i)
-        salsa20::salsa20_2(b.add(i.wrapping_mul(16)));
+        salsa20::salsa20_2(b.add(i * 16));
 
         // 12: for i = i + 1 to 2r - 1 do
         for i in (i + 1)..(2 * r) {
-            blkxor(
-                b.add(i.wrapping_mul(16usize)),
-                b.add(i.wrapping_sub(1usize).wrapping_mul(16usize)),
-                16_usize,
-            );
+            blkxor(b.add(i * 16), b.add((i - 1) * 16), 16);
             salsa20::salsa20_2(b.add(i.wrapping_mul(16)));
         }
     }
 
     /// Transform the provided block using the provided S-boxes.
     unsafe fn pwxform(&mut self, b: &mut [u32; 16]) {
-        let xptr: *mut [[u32; 2]; 2] = b.as_mut_ptr().cast();
+        let xptr: *mut [[u32; PWXSIMPLE]; 2] = b.as_mut_ptr().cast();
         let s0 = self.s0;
         let s1 = self.s1;
         let s2 = self.s2;
         let mut w = self.w;
 
         // 1: for i = 0 to PWXrounds - 1 do
-        for i in 0..6 {
+        for i in 0..PWXROUNDS {
             // 2: for j = 0 to PWXgather - 1 do
-            for j in 0..4 {
+            for j in 0..PWXGATHER {
                 let mut xl: u32 = (*xptr.add(j))[0][0];
                 let mut xh: u32 = (*xptr.add(j))[0][1];
 
                 // 3: p0 <-- (lo(B_{j,0}) & Smask) / (PWXsimple * 8)
-                let p0 = s0.add((xl as usize & (((1 << 8) - 1) * 2 * 8)) / 8);
+                let p0 = s0.add((xl as usize & SMASK) / 8);
 
                 // 4: p1 <-- (hi(B_{j,0}) & Smask) / (PWXsimple * 8)
-                let p1 = s1.add((xh as usize & (((1 << 8) - 1) * 2 * 8)) / 8);
+                let p1 = s1.add((xh as usize & SMASK) / 8);
 
                 // 5: for k = 0 to PWXsimple - 1 do
-                for k in 0..2 {
+                for k in 0..PWXSIMPLE {
                     // 6: B_{j,k} <-- (hi(B_{j,k}) * lo(B_{j,k}) + S0_{p0,k}) xor S1_{p1,k}
                     let s0 = (((*p0.add(k))[1] as u64) << 32).wrapping_add((*p0.add(k))[0] as u64);
                     let s1 = (((*p1.add(k))[1] as u64) << 32).wrapping_add((*p1.add(k))[0] as u64);
@@ -137,7 +129,7 @@ impl PwxformCtx {
                     (*xptr.add(j))[k][1] = (x >> 32) as u32;
 
                     // 8: if (i != 0) and (i != PWXrounds - 1)
-                    if i != 0 && i != (6 - 1) {
+                    if i != 0 && i != (PWXROUNDS - 1) {
                         // 9: S2_w <-- B_j
                         (*s2.add(w))[0] = x as u32;
                         (*s2.add(w))[1] = (x >> 32) as u32;
@@ -153,6 +145,6 @@ impl PwxformCtx {
         self.s2 = s1;
 
         // 15: w <-- w mod 2^Swidth
-        self.w = w & ((1 << 8) * 2 - 1);
+        self.w = w & ((1 << SWIDTH) * PWXSIMPLE - 1);
     }
 }
