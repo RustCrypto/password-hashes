b336e70e4715d075a9119d6aa03b552dd5f679e9
Tony Arcieri <bascule@gmail.com>
2025-09-09 12:31:22 -0600

    yescrypt: replace `Flags` with `Mode`
    As proposed in #694, this replaces the C-derived bitflags with a much
    simpler `Mode` enum for selecting between the three modes yescrypt
    offers: classic scrypt, write-once/read-many, and read-write.
    
    This eliminates many possible misconfigurations, including trying to use
    the `YESCRYPT_RW` mode without the appropriate flavor bits set.
    
    The flavor bits are now hardcoded. If we reintroduce some fine-grained
    control over them (we only currently support one flavor, the standard
    flavor), it would probably be good to map the various possible flavor
    settings (rounds, gather, simple, sbox) onto enums themselves.

---
+++ yescrypt/src/mode.rs
@@ -0,0 +1,83 @@
+//! yescrypt modes.
+
+use crate::{Error, Result};
+
+// Bits which represent the "flavor" of yescrypt when using `Mode::Rw`
+const ROUNDS_6: u32 = 0b000001;
+const GATHER_4: u32 = 0b000100;
+const SIMPLE_2: u32 = 0b001000;
+const SBOX_12K: u32 = 0b100000;
+
+// Bits representing the RW "flavor"
+// TODO(tarcieri): support other flavors of yescrypt?
+const RW_FLAVOR: u32 = 2 | ROUNDS_6 | GATHER_4 | SIMPLE_2 | SBOX_12K;
+
+/// yescrypt modes
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+#[repr(u32)]
+pub enum Mode {
+    /// classic scrypt
+    Classic = 0,
+
+    /// write-once/read-many: conservative enhancement of classic scrypt
+    Worm = 1,
+
+    /// yescrypt’s native mode: read-write
+    #[default]
+    Rw = RW_FLAVOR,
+}
+
+impl Mode {
+    /// Is the mode scrypt classic?
+    pub fn is_classic(self) -> bool {
+        self == Self::Classic
+    }
+
+    /// Is the mode write-once/read-many?
+    pub fn is_worm(self) -> bool {
+        self == Self::Worm
+    }
+
+    /// Is the mode the yescrypt native read-write mode? (default)
+    pub fn is_rw(self) -> bool {
+        self == Self::Rw
+    }
+}
+
+impl TryFrom<u32> for Mode {
+    type Error = Error;
+
+    fn try_from(value: u32) -> Result<Self> {
+        match value {
+            0 => Ok(Mode::Classic),
+            1 => Ok(Mode::Worm),
+            RW_FLAVOR => Ok(Mode::Rw),
+            _ => Err(Error::Params),
+        }
+    }
+}
+
+impl From<Mode> for u32 {
+    fn from(mode: Mode) -> u32 {
+        mode as u32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Mode;
+
+    #[test]
+    fn flavor() {
+        assert_eq!(0u32, Mode::Classic.into());
+        assert_eq!(1u32, Mode::Worm.into());
+        assert_eq!(0b101111u32, Mode::default().into());
+    }
+
+    #[test]
+    fn try_from() {
+        assert_eq!(Mode::try_from(0).unwrap(), Mode::Classic);
+        assert_eq!(Mode::try_from(1).unwrap(), Mode::Worm);
+        assert_eq!(Mode::try_from(0b101111).unwrap(), Mode::Rw);
+    }
+}
