a80da8d30eda16d29a7ea93bf0a730f6c8f3b0c3
Tony Arcieri <bascule@gmail.com>
2025-10-15 15:22:55 -0600

    sha-crypt: remove `std` feature; add `core::error::Error` impls
    The `std` feature was previously unused, while `core::error::Error`
    allows the error trait to be impl'd without a `std` dependency

--- sha-crypt/src/errors.rs
+++ sha-crypt/src/errors.rs
@@ -1,12 +1,10 @@
 //! Error types.
 
 use alloc::string;
+use core::fmt;
 
 #[cfg(feature = "simple")]
 use alloc::string::String;
-
-#[cfg(feature = "std")]
-use std::io;
 
 /// Error type.
 #[derive(Debug)]
@@ -17,24 +15,32 @@ pub enum CryptError {
     /// RNG failed.
     RandomError,
 
-    /// I/O error.
-    #[cfg(feature = "std")]
-    IoError(io::Error),
-
     /// UTF-8 error.
     StringError(string::FromUtf8Error),
-}
-
-#[cfg(feature = "std")]
-impl From<io::Error> for CryptError {
-    fn from(e: io::Error) -> Self {
-        CryptError::IoError(e)
-    }
 }
 
 impl From<string::FromUtf8Error> for CryptError {
     fn from(e: string::FromUtf8Error) -> Self {
         CryptError::StringError(e)
+    }
+}
+
+impl core::error::Error for CryptError {
+    fn source(&self) -> Option<&(dyn core::error::Error + 'static)> {
+        match self {
+            CryptError::StringError(err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for CryptError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CryptError::RoundsError => write!(f, "rounds error"),
+            CryptError::RandomError => write!(f, "random error"),
+            CryptError::StringError(_) => write!(f, "string error"),
+        }
     }
 }
 
@@ -55,5 +61,15 @@ pub struct DecodeError;
 impl From<DecodeError> for CheckError {
     fn from(_: DecodeError) -> CheckError {
         CheckError::InvalidFormat("invalid B64".into())
+    }
+}
+
+#[cfg(feature = "simple")]
+impl core::error::Error for DecodeError {}
+
+#[cfg(feature = "simple")]
+impl fmt::Display for DecodeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "decode error")
     }
 }
