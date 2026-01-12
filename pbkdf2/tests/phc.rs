bbbd84040eae63a6b8e792e4bd40dc024fcfc068
Tony Arcieri <bascule@gmail.com>
2026-01-11 17:45:31 -0700

    pbkdf2 v0.13.0-rc.6 (#811)

--- pbkdf2/tests/phc.rs
+++
@@ -1,6 +0,0 @@
-//! Tests for `password-hash` crate integration with Password Hashing Competition (PHC) string
-//! format as the password hash serialization.
-//!
-//! PHC PBKDF2-SHA256 vectors adapted from: https://stackoverflow.com/a/5136918
-
-#![cfg(feature = "phc")]
