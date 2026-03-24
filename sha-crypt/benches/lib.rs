5f8ed9c82aca4041dda8e125ad93c1476ccf5242
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-03-24 19:59:18 -0400

    fmt

--- sha-crypt/benches/lib.rs
+++ sha-crypt/benches/lib.rs
@@ -8,10 +8,10 @@ use test::Bencher;
 
 #[bench]
 pub fn sha_crypt_sha_512(bh: &mut Bencher) {
-    let input = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; 
+    let input = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
     let salt = b"idU8Ptdv2tVGArtN";
     let rounds = 5_000;
-    let sha_crypt = ShaCrypt::new(Algorithm::Sha256Crypt, Params::new(rounds).unwrap());    
+    let sha_crypt = ShaCrypt::new(Algorithm::Sha256Crypt, Params::new(rounds).unwrap());
 
     bh.iter(|| {
         sha_crypt.hash_password_with_salt(input, salt).unwrap();
@@ -20,10 +20,10 @@ pub fn sha_crypt_sha_512(bh: &mut Bencher) {
 
 #[bench]
 pub fn sha_crypt_sha_256(bh: &mut Bencher) {
-    let input = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; 
+    let input = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
     let salt = b"idU8Ptdv2tVGArtN";
     let rounds = 5_000;
-    let sha_crypt = ShaCrypt::new(Algorithm::Sha512Crypt, Params::new(rounds).unwrap());    
+    let sha_crypt = ShaCrypt::new(Algorithm::Sha512Crypt, Params::new(rounds).unwrap());
 
     bh.iter(|| {
         sha_crypt.hash_password_with_salt(input, salt).unwrap();
